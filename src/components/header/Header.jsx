d79def1c224088c0ca90e1d880f0673c57fdcc89
Florian <bouclet.florian@hotmail.com>
2022-10-17 23:04:33 +0200

    création du header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HearderSocials from './HearderSocials'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <Header>
+      <div className="container header__container">
+        <h5>Bonjour je suis</h5>
+        <h1>Florian Bouclet</h1>
+        <h5 className='text-light'>Développeur Web - Frontend</h5>
+        <CTA />
+        <HearderSocials />
+
+        <div className="me">
+          <img src={ME} alt="Photo de profil" />
+        </div>
+
+        <a href='#contact' className='scroll__down'>Bas de page</a>
+      </div>
+    </Header>
   )
 }
 
