4b2f1b87332b42a51a19a2a12722e5f57dbe1b43
Florian <bouclet.florian@hotmail.com>
2022-10-22 11:28:27 +0200

    création de la nav et about-section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,49 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/me-about1.jpg'
+import {FaAward} from 'react-icons/fa'
+import {FiUsers} from 'react-icons/fi'
+import {VscFolderLibrary} from 'react-icons/vsc'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section id='about'>
+      <h5>Faire Connaissance</h5>
+      <h2>A propos de moi</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME}/>
+          </div>
+        </div>
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <FaAward className='about_icon'/>
+              <h5>Experience</h5>
+              <small>3 ans de travail</small>
+            </article>
+            <article className="about__card">
+              <FiUsers className='about_icon'/>
+              <h5>Clients</h5>
+              <small>5+ clients</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className='about_icon'/>
+              <h5>Projets</h5>
+              <small>10+ réalisés</small>
+            </article>
+          </div>
+
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Unde quis in neque, perferendis aut, consectetur iste delectus inventore ullam possimus eligendi adipisci voluptatum repudiandae beatae veniam rerum, nisi enim nihil.
+          </p>
+
+          <a href='#contact'className='btn btn-primary'>En savoir +</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
