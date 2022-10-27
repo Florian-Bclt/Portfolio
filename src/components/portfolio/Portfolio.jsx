758d31b416952759f1f72ec75510822df9dced16
Florian <bouclet.florian@hotmail.com>
2022-10-27 23:21:28 +0200

    ajout du projet adminfit

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
+import IMG2 from '../../assets/portfolio2.png'
 import IMG3 from '../../assets/portfolio3.jpg'
 import IMG4 from '../../assets/portfolio4.jpg'
 import IMG5 from '../../assets/portfolio5.png'
@@ -18,7 +18,7 @@ const data = [
   {  
     id: 2,
     image: IMG2,
-    title: 'Charts templates & infographics in Figma',
+    title: "Admin-Fit - Dashboard de gestion d'utilisateurs",
     github: 'https://github.com',
     demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
   },
