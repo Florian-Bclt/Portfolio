de6a0bc2cb0716b845d0428c2a55d7690c4d2592
Florian <bouclet.florian@hotmail.com>
2023-10-25 18:25:43 +0200

    edit backgroundd

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import {SiReact, SiNestjs, SiGraphql, SiWordpress, SiHtml5, SiCss3, SiJavascript, SiRedux} from 'react-icons/si'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.png'
