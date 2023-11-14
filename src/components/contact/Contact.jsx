bb7f307c51c323925fdb881d9cfed189c84cd412
Florian <bouclet.florian@hotmail.com>
2023-11-14 22:11:50 +0100

    edit contact

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,6 @@
-import React, {useRef} from 'react'
+import React, {useRef, useState} from 'react'
 import './contact.css'
-import {MdOutlineEmail}from 'react-icons/md'
+import {MdOpacity, MdOutlineEmail}from 'react-icons/md'
 import {RiMessengerLine}from 'react-icons/ri'
 import {BsWhatsapp}from 'react-icons/bs'
 import emailjs from 'emailjs-com'
@@ -11,13 +11,29 @@ AOS.init();
 
 const Contact = () => {
   const form = useRef();
+  const [validationMessage, setValidationMessage] = useState('');
+  const [validationColor, setValidationColor] = useState('');
 
-  const sendEmail = (e) => {
+  const sendEmail = async (e) => {
     e.preventDefault();
+    try {
+      const result = await emailjs.sendForm('service_08k76hg', 'template_vi8jqxi', form.current, 'c9BlF0wItDrqYhC8l');
 
-    emailjs.sendForm('service_15spssq', 'template_5xn4kar', form.current, 'OXe1BccbCa-DlLyTl')
+      if (result) {
+        setValidationMessage('Succès ! Message envoyé.');
+        setValidationColor('#27ae60');
+      } else {
+        setValidationMessage('Erreur ! As-tu bien rempli chacun des champs ?');
+        setValidationColor('#8a1212')
+      }
+    } catch (error) {
+      console.error('Erreur lors de l\'envoi du formulaire : ', error);
+      setValidationMessage('Erreur ! As-tu bien rempli chacun des champs ?');
+      setValidationColor('#8a1212');    
+    }
 
-    e.target.reset()
+    // Réinitialiser le formulaire
+    e.target.reset();
   };
 
 
@@ -31,8 +47,8 @@ const Contact = () => {
           <article className='contact__option'data-aos="zoom-in">
             <MdOutlineEmail className='contact__option-icon'/>
             <h4>Email</h4>
-            <h5 className='text-light'>bouclet.florian@hotmail.com</h5>
-            <a href="mailto:bouclet.florian@hotmail.com" target='_blank'>Envoyer un message</a>
+            <h5 className='text-light'>bouclet.florian@gmail.com</h5>
+            <a href="mailto:bouclet.florian@gmail.com" target='_blank'>Envoyer un message</a>
           </article>
 
           <article className='contact__option' data-aos="zoom-in" data-aos-delay="200">
@@ -56,7 +72,21 @@ const Contact = () => {
           <input type="email" name="email" placeholder='Votre Email' required data-aos='zoom-in' data-aos-delay="500" />
           <textarea name="message" rows="7" placeholder='Votre message' required data-aos='zoom-in' data-aos-delay="700"></textarea>
           <button type='submit' className='btn btn-primary' data-aos='zoom-in' data-aos-delay="900">Envoyer</button>
+            {/* Affichage du message de validation */}
+            {validationMessage && (
+              <div style={{ 
+                color: validationColor, 
+                padding: '2px',
+                display: 'flex',
+                justifyContent: 'center',
+                textAlign: 'center',
+                borderRadius: '10px',
+                }}>
+                {validationMessage}
+              </div>
+            )}
         </form>
+
       </div>
     </section>
   )
