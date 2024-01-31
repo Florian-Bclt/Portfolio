3988a32c331fb7b1ea3a90ab94ecb3cf79c420f6
Florian <bouclet.florian@hotmail.com>
2024-01-31 21:47:56 +0100

    edit logo

--- src/components/header/HearderSocials.jsx
+++ src/components/header/HearderSocials.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import {BsLinkedin} from 'react-icons/bs'
 import {FaGithub} from 'react-icons/fa'
-import {FiDribbble} from 'react-icons/fi'
 import images from '../../constants/images'
 
 const HearderSocials = () => {
