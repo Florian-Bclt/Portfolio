bd6247c0926deb1730b2854324e0b61e0fae3ba2
Florian <bouclet.florian@hotmail.com>
2025-01-21 15:10:06 +0100

    fix coquille

--- src/components/tarif/Maintenance.jsx
+++ src/components/tarif/Maintenance.jsx
@@ -13,7 +13,7 @@ const Maintenance = () => {
           <img src={images.maintenance} alt="application dynamique" />
           <div className="tarif__static__desc">
             <h2>Maintenance</h2>
-            <h4>Site vitrine : 30€ HT/mois<br/>Site dynamqiue :  dès 45€ HT/mois</h4>
+            <h4>Site vitrine : 30€ HT/mois<br/>Site dynamique :  dès 45€ HT/mois</h4>
             <p>Mon service de maintenance comprend des mises à jour régulières, la correction rapide des bugs et l'intégration des dernières évolutions technologiques. Je veille à ce que votre site soit toujours sécurisé, performant et conforme aux nouvelles normes, y compris le respect de la loi RGPD et des recommandations de la CNIL. Confiez-moi la maintenance de votre site et concentrez-vous sur le développement de votre activité, en sachant que votre présence en ligne est entre de bonnes mains.</p>
             <div className="tarif__navigation">
               <Link to='/webmaster'><button className='btn btn-primary'>En savoir plus sur le service Webmaster</button></Link>
